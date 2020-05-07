88aa05639aae753c593ba60ee6054d811f072ce1
黄锦坚 <huangjinjian@pcitech.com>
2020-05-07 10:03:03 +0800

    基本 已完成

--- springcloud/common/src/main/java/com/hl/common/util/JwtHelper.java
+++ springcloud/common/src/main/java/com/hl/common/util/JwtHelper.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
